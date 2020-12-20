e8a6f76a601e92985a31243106127bdc2b368e1f
Sandra Shtabnaya <aleksandra.shtabnaya@gmail.com>
2020-12-20 15:16:37 -0500

    Add percentile chart.

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,39 +1,28 @@
 /* globals Chart:false, feather:false */
-
 (function () {
     'use strict'
   
     feather.replace()
   
     // Graphs
-    
-
-    var ctx = document.getElementById("histogram").getContext('2d');
-    var dataValues = [801, 437, 412, 300, 70, 27, 100, 10, 24, 34, 15,
+    var histId = document.getElementById("histogram").getContext('2d');
+    var histValues = [801, 437, 412, 300, 70, 27, 100, 10, 24, 34, 15,
                       17, 23, 80, 4, 7, 2, 3, 0, 2, 6, 1,
                       0, 7, 0, 2, 5, 1, 1, 0, 2, 0, 1];
-    var dataLabels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    var histLabels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                       11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
                       21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
-    var myChart = new Chart(ctx, {
+    var histogram = new Chart(histId, {
       type: 'bar',
       data: {
-        labels: dataLabels,
+        labels: histLabels,
         datasets: [{
           label: 'Total Projects',
-          data: dataValues,
+          data: histValues,
           backgroundColor: 'rgb(13, 110, 253)',
         }]
       },
       options: {
-        title: {
-          display: false,
-          position: 'top',
-          fontsize: 20,
-          fontcolor: 'rgb(13, 110, 253)',
-          padding: 50,
-          text: 'Round SSS Trends'
-        },
         scales: {
           xAxes: [{
             display: false,
@@ -71,5 +60,41 @@
         }
       }
     })
+
+    var percId = document.getElementById("percentile").getContext('2d');
+    var percValues = [0.149, 0.283, 0.737, 2.301, 7.347, 25.141, 44.623];
+    var percLabels = [5, 10, 25, 50, 75, 90, 95]
+    var percentiles = new Chart(percId, {
+      type: 'line',
+      data: {
+        labels: percLabels,
+        datasets: [{
+          label: 'SSS',
+          data: percValues,
+          borderColor: 'rgb(13, 110, 253)',
+          backgroundColor: 'rgba(13, 109, 253, 0.293)'
+        }]
+      },
+      options: {
+        scales: {
+          xAxes: [{
+            gridLines: {
+              display:false
+            }  
+          }]
+        },
+        legend: {
+          display: false
+        },
+        layout: {
+          padding: {
+              left: 30,
+              right: 30,
+              top: 30,
+              bottom: 30
+          }
+        }
+      }
+    })
 })()
-  
+
