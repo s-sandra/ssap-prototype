0b66bf3364f68c1bf5205735024d622c40ccdd3c
Sandra Shtabnaya <aleksandra.shtabnaya@gmail.com>
2020-12-19 21:38:54 -0500

    Add histogram data and adjust styles.

--- scripts/chart.js
+++ scripts/chart.js
@@ -9,14 +9,18 @@
     
 
     var ctx = document.getElementById("histogram").getContext('2d');
-    var dataValues = [12, 19, 3, 5];
-    var dataLabels = [0, 1, 2, 3, 4];
+    var dataValues = [801, 437, 412, 300, 70, 27, 100, 10, 24, 34, 15,
+                      17, 23, 80, 4, 7, 2, 3, 0, 2, 6, 1,
+                      0, 7, 0, 2, 5, 1, 1, 0, 2, 0, 1];
+    var dataLabels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                      11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+                      21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
     var myChart = new Chart(ctx, {
       type: 'bar',
       data: {
         labels: dataLabels,
         datasets: [{
-          label: 'SSS',
+          label: 'Total Projects',
           data: dataValues,
           backgroundColor: 'rgb(13, 110, 253)',
         }]
@@ -33,21 +37,25 @@
         scales: {
           xAxes: [{
             display: false,
-            barPercentage: 1.3,
+            barPercentage: 1.29,
             ticks: {
-                max: 3,
+                beginAtZero: true,
+                max: 29, // the max number of bars
             }
-        }, {
+          },{
             display: true,
             ticks: {
                 autoSkip: false,
-                max: 4,
-            }
+                max: 30,
+            },
+            gridLines: {
+              display:false
+            }  
           }],
           yAxes: [{
             ticks: {
               beginAtZero: true
-            }
+            } 
           }]
         },
         legend: {
@@ -57,10 +65,10 @@
           padding: {
               left: 30,
               right: 30,
-              top: 60,
-              bottom: 50
+              top: 30,
+              bottom: 30
+          }
         }
-      }
       }
     })
 })()
